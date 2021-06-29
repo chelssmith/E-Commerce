cd96b43e366718dd1b708b95a1df9b12428bbfb7
Chelsea Smith <smith2chelsea2@gmail.com>
2021-06-28 20:57:11 -0400

    more routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,7 +55,7 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const categoryData = await Category.destroy({
       where: {
